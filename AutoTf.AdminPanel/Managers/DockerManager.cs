0aa1f3977d84305d9fa084d8e76122dd5107f6c2
chiouya <chiouyazo@gmail.com>
2025-04-30 13:03:02 +0200

    Added exist check to several docker methods

--- AutoTf.AdminPanel/Managers/DockerManager.cs
+++ AutoTf.AdminPanel/Managers/DockerManager.cs
@@ -13,6 +13,7 @@ public class DockerManager
         _dockerClient = new DockerClientConfiguration(new Uri("unix:///var/run/docker.sock")).CreateClient();
     }
 
+    // TODO: Cache?
     public async Task<List<ContainerListResponse>> GetContainers()
     {
         IList<ContainerListResponse>? containers = await _dockerClient.Containers.ListContainersAsync(new ContainersListParameters()
@@ -24,6 +25,11 @@ public class DockerManager
         return containers.Where(x => x.Labels.ContainsKey("com.docker.compose.project") && x.Labels["com.docker.compose.project"] == "centralserver").ToList();
     }
 
+    public async Task<bool> DoesContainerExist(string id)
+    {
+        return (await GetContainers()).Any(x => x.ID == id);
+    }
+
     public async Task<CreateContainerResponse> CreateContainer(CreateContainer parameters, Dictionary<string, EndpointSettings> networks)
     {
         
@@ -32,6 +38,9 @@ public class DockerManager
         
         foreach (KeyValuePair<string, string> portMapping in parameters.PortMappings)
         {
+            if (portMapping.Key == "" || portMapping.Value == "")
+                throw new Exception("Ports were empty.");
+            
             string hostPort = portMapping.Key;
             string containerPort = portMapping.Value;
 
@@ -69,16 +78,25 @@ public class DockerManager
     
     public async Task<bool> StartContainer(string containerId)
     {
+        if (!await DoesContainerExist(containerId))
+            return false;
+        
         return await _dockerClient.Containers.StartContainerAsync(containerId, new ContainerStartParameters());
     }
 
     public async Task<bool> StopContainer(string containerId)
     {
+        if (!await DoesContainerExist(containerId))
+            return false;
+        
         return await _dockerClient.Containers.StopContainerAsync(containerId, new ContainerStopParameters());
     }
 
     public async Task DeleteContainer(string containerId)
     {
+        if (!await DoesContainerExist(containerId))
+            return;
+        
         await _dockerClient.Containers.RemoveContainerAsync(containerId, new ContainerRemoveParameters());
     }
 
