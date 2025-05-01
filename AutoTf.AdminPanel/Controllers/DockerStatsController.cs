81b9545965457f19dd305064b62d518e877b70ee
chiouya <chiouyazo@gmail.com>
2025-05-01 13:17:58 +0200

    Added stats controller

--- AutoTf.AdminPanel/Controllers/DockerStatsController.cs
+++ AutoTf.AdminPanel/Controllers/DockerStatsController.cs
@@ -1,6 +1,23 @@
+using AutoTf.AdminPanel.Managers;
+using AutoTf.AdminPanel.Models.Requests;
+using Microsoft.AspNetCore.Mvc;
+
 namespace AutoTf.AdminPanel.Controllers;
 
-public class DockerStatsController
+[ApiController]
+[Route("/api/docker/stats")]
+public class DockerStatsController : ControllerBase
 {
-    
+    private readonly DockerStatsManager _docker;
+
+    public DockerStatsController(DockerStatsManager docker)
+    {
+        _docker = docker;
+    }
+
+    [HttpGet("memory")]
+    public async Task<ActionResult<MemoryStats>> Memory()
+    {
+        return await _docker.Memory();
+    }
 }
