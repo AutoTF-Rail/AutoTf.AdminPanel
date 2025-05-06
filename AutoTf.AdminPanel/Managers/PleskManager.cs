76416f0e5adf898d762f2dbc9f71b01e7824786b
chiouya <chiouyazo@gmail.com>
2025-05-06 09:41:13 +0200

    Removed docker files and updated regex pattern for plesk

--- AutoTf.AdminPanel/Managers/PleskManager.cs
+++ AutoTf.AdminPanel/Managers/PleskManager.cs
@@ -9,7 +9,7 @@ namespace AutoTf.AdminPanel.Managers;
 public class PleskManager : IHostedService
 {
     private const string _authHostPattern = "(?:http|https)://\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}(?::\\d{1,6})?";
-    private const string _domainsPattern = "((?:[a-z0-9-]+\\.)*)([a-z0-9-]+\\.[a-z]{2,})";
+    private const string _domainsPattern = "((?:[a-z0-9-]+)(?:\\.[a-z0-9-]+)*)([a-z0-9-]+\\.[a-z]{2,})";
     
     private Timer _timer = new Timer(TimeSpan.FromMinutes(5));
 
--- publishScript.sh
+++
@@ -1,5 +0,0 @@
-# This script is used on the server that hosts the central servers and admin panel.
-dotnet publish -c Release -o out
-docker login 172.17.0.3:5001
-docker build -t 172.17.0.3:5001/repository/docker-autotf/autotf.adminpanel:latest .
-docker push 172.17.0.3:5001/repository/docker-autotf/autotf.adminpanel:latest
